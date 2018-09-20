1fd7465d99bbc212c760e7ed64d834b65aa2f823
emilMircea <emil-mircea@protonmail.com>
2018-09-20 19:59:32 +0200

    remove unnecessary jsx

--- src/components/Navbar/Message.js
+++ src/components/Navbar/Message.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const Message = props => (
-	<li className="nav-link">
-		<a href="#" className="nav-item dropdown-item" style={{fontSize: '0.7em'}}>
-			{props.message}
-		</a>
-	</li>
+	<a href="#" className="nav-item dropdown-item" style={{fontSize: '0.7em'}}>
+		{props.message}
+	</a>
+	
 )
 
 export default Message
--- src/components/SideBar/Sidebar.js
+++ src/components/SideBar/Sidebar.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const SideBarWrapper = styled.ul`
-	background: linear-gradient(to bottom, #e14eca 0%, #ba54f5 100%);
-	height: 90%;
-	border-radius: 0.4rem;
-`
 
 const Sidebar = () => (
-	<SideBarWrapper className="nav flex-column">
+	<ul className="nav flex-column sidebar">
 		<li className="nav-item">
 			<a href="#" className="nav-link active">Some Link</a>
 		</li>
@@ -21,8 +14,7 @@ const Sidebar = () => (
 		<li className="nav-item">
 			<a href="#" className="nav-link">Some Link</a>
 		</li>
-
-	</SideBarWrapper>
+	</ul>
 )
 
 export default Sidebar
