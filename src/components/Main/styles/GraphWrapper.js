4c3edb35325ebac7efc309802aca9d2495fd3551
emilMircea <emil-mircea@protonmail.com>
2018-09-20 19:58:36 +0200

    add style

--- src/components/Main/styles/GraphWrapper.js
+++ src/components/Main/styles/GraphWrapper.js
@@ -2,5 +2,7 @@ import styled from 'styled-components'
 
 const GraphWrapper = styled.div`
   background: #27293d;
-`
+  padding: 2rem 0;
+  border: 5px solid #1e1e28;
+`;
 export default GraphWrapper
