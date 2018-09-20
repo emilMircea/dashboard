fbbe2b80c40d12c9c030b34f7ecdacb08c0241e8
emilMircea <emil-mircea@protonmail.com>
2018-09-20 19:58:24 +0200

    add fake data and chartjs bar chart

--- src/components/Main/MonthlySales.js
+++ src/components/Main/MonthlySales.js
@@ -1,17 +1,49 @@
 import React from 'react'
 import GraphWrapper from './styles/GraphWrapper'
 import GraphHeader from './styles/GraphHeader'
-import GraphBody from './styles/GraphBody'
+import { Bar } from 'react-chartjs-2'
+import CustomIconComponent from './CustomIconComponent'
+import deliveryIcon from "../../assets/images/delivery-icon.svg"
+
+const data = (canvas) => {
+	const ctx = canvas.getContext("2d")
+	let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+	gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+	gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+	gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+	return { 
+		labels: ["Week 1", "Week 2", "Week 3", "Week 4"], 
+		datasets: [{ label: "Sold this month",
+		borderColor: "#3596ff", 
+ 		pointBorderColor: "#FFF", 
+		pointBackgroundColor: "#3596ff", 
+		pointBorderWidth: 2, 
+		pointHoverRadius: 4, 
+		pointHoverBorderWidth: 1, 
+		pointRadius: 4, 
+		fill: true, 
+		backgroundColor: gradientStroke, 
+		borderWidth: 2,
+		suggestedMin: 100,
+		suggestedMax: 500,
+		data: [152, 150, 148, 180] }] 	
+	};
+}
 
 const MonthlySales = () => (
-	<GraphWrapper className="col-4 card card-chart">
-		<GraphHeader className="card-header">
-			<h9 style={{color: '#9A9A9A'}} className='card-category'>Monthly Sales</h9>
-		</GraphHeader>
-		<GraphBody className="card-body">
-			<p>body of Sales</p>
-		</GraphBody>
-	</GraphWrapper>
-)
+  <GraphWrapper className="col-4 card card-chart">
+    <GraphHeader className="card-header">
+      <span style={{ color: "#9A9A9A" }} className="card-category">
+        Monthly Sales
+      </span>
+			<h5 className="card-title graph-title">
+				<CustomIconComponent icon={deliveryIcon} total={189} />
+			</h5>
+    </GraphHeader>
+    
+    <Bar width={350} height={290} data={data} />
+    
+  </GraphWrapper>
+);
 
 export default MonthlySales
