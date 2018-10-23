5da598027a913f7f75fc40ea4d126b3b856e4ce1
emilMircea <emil-mircea@protonmail.com>
2018-10-23 15:32:06 +0300

    add animation for components and prettier format

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import MainPanel from './components/Main/MainPanel'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/SideBar/Sidebar'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import 'animate.css'
+import "./App.css"; 
+import MainPanel from "./components/Main/MainPanel";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/SideBar/Sidebar";
+import styled from "styled-components";
 
 const NavbarStyle = styled.ul`
-	display: flex;
-	justify-content: space-between;
-	padding-left: 2rem;
-	align-items: center;
-	margin-bottom: 1rem;
-`
+  display: flex;
+  justify-content: space-between;
+  padding-left: 2rem;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
 
 class App extends Component {
-	render() {
-		return <div className="container-fluid">
+  render() {
+    return <div className="container-fluid">
         <NavbarStyle className="nav">
-          <li className="nav-item">
-            <a className="navbar-brand" style={{ color: "white" }}>
-              Logo
-            </a>
-          </li>
           <Navbar />
         </NavbarStyle>
-
         <div className="row">
-          <div className="col-2">
+        <div className="col-2 animated slideInLeft faster">
             <Sidebar />
           </div>
           <div className="col-10">
             <MainPanel />
           </div>
         </div>
-        <div className='footer'>
+        <div className="footer">
           <p>footer info</p>
         </div>
       </div>;
-	}
+  }
 }
 
-export default App
+export default App;
