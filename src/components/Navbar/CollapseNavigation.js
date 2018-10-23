62a6a524a95da5530abde9d94dda6253ee6a3f51
emilMircea <emil-mircea@protonmail.com>
2018-10-23 15:32:27 +0300

    add prettier format

--- src/components/Navbar/CollapseNavigation.js
+++ src/components/Navbar/CollapseNavigation.js
@@ -1,71 +1,54 @@
-import React, { PureComponent } from 'react'
-// import Notification from './Notification'
-import Message from './Message'
-import { Collapse } from 'react-collapse'
-import { IconContext } from 'react-icons'
-import { IoMdNotificationsOutline } from 'react-icons/io'
-import styled from 'styled-components'
-
+import React, { PureComponent } from "react";
+import Message from "./Message";
+import { Collapse } from "react-collapse";
+import { IconContext } from "react-icons";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import styled from "styled-components";
+import messages from "../../data/messajes";
 
 const MessagesLength = styled.p`
-	border-radius: 32px;
-	background: #fd5d93;
-	padding: .1rem .4rem;
-	font-size: .7rem;
-	font-weight: 500;
-	color: #ebff00;
-`
-
-const messages = [
-	{
-		content: 'You have 3 returned items'
-	},
-	{
-		content: 'Prices of several items have changed'
-	},
-	{
-		content: 'You have 4 customer emails'
-	},
-	{
-		content: 'The monthly expenses have been computed! See them here 👌'
-	}
-]
+  border-radius: 32px;
+  background: #fd5d93;
+  padding: 0.1rem 0.4rem;
+  font-size: 0.7rem;
+  font-weight: 500;
+  color: #ebff00;
+`;
 
 class CollapseNavigation extends PureComponent {
-	state = {
-		isOpened: false
-	}
+  state = {
+    isOpened: false
+  };
 
-	handleOpenMessageList = () => {
-		this.setState({
-			isOpened: !this.state.isOpened
-		})
-	}
+  handleOpenMessageList = () => {
+    this.setState({
+      isOpened: !this.state.isOpened
+    });
+  };
 
-	render() {
-		return (
-				<div onClick={this.handleOpenMessageList} style={{ display: 'flex' }}>
-					<IconContext.Provider value={{ className: 'icon-large' }}>
-						<IoMdNotificationsOutline />
-					</IconContext.Provider>
-					<MessagesLength>{messages.length}</MessagesLength>
-					<Collapse isOpened={this.state.isOpened}>
-						<ul
-							className={`dropdown-menu dropdown-menu-right dropdown-navbar ${
-								this.state.isOpened ? 'show' : null
-							}`}
-						>
-							{messages.map((message, index) => (
-								<li key={index}>
-									<Message message={message.content} />
-								</li>
-							))}
-						</ul>
-					</Collapse>
-				</div>
-			
-		)
-	}
+  render() {
+    return (
+      <div onClick={this.handleOpenMessageList} className='notifications-wrapper'>
+        <IconContext.Provider value={{ className: "icon-large" }}>
+          <IoMdNotificationsOutline />
+        </IconContext.Provider>
+        <MessagesLength>{messages.length}</MessagesLength>
+        <Collapse isOpened={this.state.isOpened}>
+          <ul
+            className={`dropdown-menu dropdown-menu-right dropdown-navbar ${
+              this.state.isOpened ? "show" : null
+            }`}
+          >
+            {messages.map((message, index) => (
+              <li key={index}>
+                <Message message={message.content} />
+              </li>
+            ))}
+          </ul>
+        </Collapse>
+      </div>
+    );
+  }
 }
 
-export default CollapseNavigation
+export default CollapseNavigation;
