5b293e2b00dc8527c77884314b7bd5a965875aa1
emilMircea <emil-mircea@protonmail.com>
2018-10-23 15:29:32 +0300

    remove logo link and add styling remains just a parag

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import CollapseNavigation from './CollapseNavigation'
+import React from "react";
+import CollapseNavigation from "./CollapseNavigation";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+`;
 
 const Navbar = () => (
-	<nav className="navbar navbar-expand-lg navbar-absolute navbar-transparent">
-		<CollapseNavigation />
-	</nav>
-)
+  <Nav className="navbar navbar-expand-lg navbar-absolute navbar-transparent">
+    <p className="navbar-brand" style={{ color: "white" }}>
+      ACME Logo
+    </p>
+    <li>
+     <CollapseNavigation />
+    </li>
+  </Nav>
+);
 
-export default Navbar
+export default Navbar;
